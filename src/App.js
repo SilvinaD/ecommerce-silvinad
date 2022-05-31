366fe38cfdc829040a23e28e5513df2d8b1fa80f
SilvinaD <silvinadonvito21@gmail.com>
2022-05-31 20:06:46 -0300

    Nabvar basico

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import Cuerpo from './components/Cuerpo/Cuerpo';
 
 function App() {
+  //Todo lo que se ve en pantalla - JS parecido a html - Dif claseName y cierre /
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+      <NavBar/>
+      <Cuerpo/>
+   </>
   );
 }
 
---
+++ src/components/Cuerpo/Cuerpo.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg'
+
+function Cuerpo(){
+    return (
+
+      <h1>Productos a definir</h1>
+
+     )
+}
+
+
+export default Cuerpo;
