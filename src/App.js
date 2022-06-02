21ed76e63e5bd40b5d5bf70b36e2335cc7170569
SilvinaD <silvinadonvito21@gmail.com>
2022-06-02 18:06:20 -0300

    Creo componente ItelListContainer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-import Cuerpo from './components/Cuerpo/Cuerpo';
+//import Cuerpo from './components/Cuerpo/Cuerpo';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   //Todo lo que se ve en pantalla - JS parecido a html - Dif claseName y cierre /
   return (
    <>
       <NavBar/>
-      <Cuerpo/>
+      {/* <Cuerpo/> */}
+      <ItemListContainer greeting='Bienvenidos !!!!!'/>
    </>
   );
 }
---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,18 @@
+//function ItemListContainer() {
+  //  return(
+
+    //    <h1>Aca va el catalogo</h1>
+
+    //)
+//}
+
+const ItemListContainer = ({greeting}) => {
+   
+    return (
+        
+            <h2>{greeting}</h2>
+      
+    );
+}
+
+export default ItemListContainer;
