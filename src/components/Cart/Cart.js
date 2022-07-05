1bf75bcb4399ad5905347de8c28edcaddeeb1f10
SilvinaD <silvinadonvito21@gmail.com>
2022-07-04 22:44:58 -0300

    Quitando Alerts

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,13 @@
-import { addDoc, collection, getFirestore, serverTimestamp } from "firebase/firestore";
+import {addDoc, collection, getFirestore, serverTimestamp} from "firebase/firestore";
+import Swal from 'sweetalert2';
 
-import { UseCartContext } from "../../context/CartContext";
+import {UseCartContext} from "../../context/CartContext";
 import CartItem from "./CartItem";
 
 
   const Cart = () => {
 
-    const { CartList, EmptyCart, PriceTotal, IconCart } = UseCartContext();
+    const {CartList, EmptyCart, PriceTotal, IconCart} = UseCartContext();
   
     const createOrder = () => {
 
@@ -14,19 +15,19 @@ import CartItem from "./CartItem";
         const queryCollection = collection(db, "orders")
         const total = PriceTotal()
 
-        const user = { nombre: "Silvina Donvito", email: "silvina1@gmail.com", phone: "54112584691" }
+        const user = {nombre: "Silvina Donvito", email: "silvina1@gmail.com", phone: "54112584691"}
 
-        const order = { user, CartList, total, created_at: serverTimestamp() }
+        const order = {user, CartList, total, created_at: serverTimestamp()}
 
         const request = addDoc(queryCollection, order)
 
         request
             .then((resp) => {
-                alert("Your purchase was succesful!" + resp.id);
+               Swal.fire("Your purchase was succesful!", 'Order number:  ' + resp.id,  'success');
 
-            })
+            }) 
             .catch((error) => {
-                console.log(": " + error)
+                console.log("An error ocurred: " + error)
             })
             .finally((resp)=> {
                 EmptyCart();
@@ -37,17 +38,17 @@ import CartItem from "./CartItem";
   return (
     <>
     <div>
-      { CartList.length < 1 ? ( 
+      {CartList.length < 1 ? ( 
         <p>Empty Cart</p>
       ) : (
-        CartList.map((i) => <CartItem key={i.item.id} item ={i.item} counter= {i.counter }/>)
+        CartList.map((i) => <CartItem key={i.item.id} item ={i.item} counter= {i.counter}/>)
       ) }
     </div>
     
-    <button onClick={ EmptyCart }>Clean Cart</button>
-    <p>Total amount { PriceTotal() } </p>
-    { IconCart() < 1 ? <p> </p> : <p> Item { IconCart() } </p> }
-    <button onClick={ createOrder }>Confirm order</button>
+    <button onClick={EmptyCart}>Clean Cart</button>
+    <p>Total amount {PriceTotal()} </p>
+    {IconCart() < 1 ? <p> </p> : <p> Item { IconCart() } </p>}
+    <button onClick={createOrder}>Confirm order</button>
 
     </>
   );
