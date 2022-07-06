20906a0a8a8fc931bfd1aa9c30d0cedcf3d82bfd
SilvinaD <silvinadonvito21@gmail.com>
2022-07-05 21:02:27 -0300

    Opcion b si no funciona git va imagen 404.jpg

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -5,8 +5,9 @@ const NotFound = () => {
   return (
     <div>
       <h1>404</h1>
-      <h5> Sorry, we can't seem to find the page you're looking for.</h5>
-      <img src={'../assets/images/GiftNotFound.mp4'} alt=" " />
+      <h5> Sorry, we can't seem to find the page you're looking for.</h5> 
+      {/*<img src={'../assets/images/404.jpg'} alt=" " />*/}
+      <img src={'../assets/images/GiftNotFound.mp4'} alt=" " />  
       <Link to={"/"} >
            <button> Go Back </button>
       </Link>
