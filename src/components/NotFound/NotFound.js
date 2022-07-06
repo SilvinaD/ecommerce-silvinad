935d6b42dd30d267dd33419dff0155d0867bf65e
SilvinaD <silvinadonvito21@gmail.com>
2022-07-06 00:20:27 -0300

    Probando estilo en notFound

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,17 +1,20 @@
+import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom"
-//import {loadingGift} from '../assets/images/GiftNotFound.mp4'
+//import {Gift404} from '../../assets/images/GiftNotFound.mp4'
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404</h1>
-      <h5> Sorry, we can't seem to find the page you're looking for.</h5> 
-      {/*<img src={'../assets/images/404.jpg'} alt=" " />*/}
-      <img src={'../assets/images/GiftNotFound.mp4'} alt=" " />  
-      <Link to={"/"} >
-           <button> Go Back </button>
-      </Link>
-    </div>
+      <div className="text-center">
+          <h1>404</h1>
+          <h5>Sorry, we can't seem to find the page you're looking for.</h5> 
+     
+          {/*<img src={'../assets/images/404.jpg'} alt=" " />*/}
+          <img src={'../../assets/images/GiftNotFound.mp4'} alt=" " />  
+    
+          <Link to={"/"} >
+          <Button variant='outline-dark' size='lg' > Go Back </Button>
+          </Link>
+        </div>
   );
 };
 
