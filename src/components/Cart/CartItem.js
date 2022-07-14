c85c1c42b574db9e0333d2e7f2f3bbdaaa70495f
SilvinaD <silvinadonvito21@gmail.com>
2022-07-13 21:30:08 -0300

    ajustes tab

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -11,23 +11,22 @@ const CartItem = ({item, counter}) => {
       <MDBCard style={{ maxWidth: '22rem' }}>
       <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
         <MDBCardImage src={ image } fluid alt='...' />
-        <a>
-          <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
-        </a>
+          <a>
+            <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+          </a>
       </MDBRipple>
       <MDBCardBody>
         <MDBCardTitle>{ title }</MDBCardTitle>
-        <MDBCardText>
-          Price: USD {price * counter}
-        </MDBCardText>
-        <MDBCardText>
-          Units: {counter}
-        </MDBCardText>
+          <MDBCardText>
+            Price: USD {price * counter}
+          </MDBCardText>
+          <MDBCardText>
+            Units: {counter}
+          </MDBCardText>
         <MDBBtn color='outline-danger'
- onClick={() => DelItem (id)}>Delete</MDBBtn>
+            onClick={() => DelItem (id)}>Delete</MDBBtn>
       </MDBCardBody>
-    </MDBCard>
-
+      </MDBCard>
     </div>
   )
 }
